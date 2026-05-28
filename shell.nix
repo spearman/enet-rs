9d447f25a45a0b31d2e39f9c6bbb717adf5fb2c9
Shane Pearman <spearman@gitlab.com>
2026-05-28 22:43:01 +0200

    remove rust-analyzer from shell.nix, check for rg command in pre-push githook

--- shell.nix
+++ shell.nix
@@ -7,7 +7,6 @@ mkShell {
     clang # required to compile enet-sys
     cmake # required to compile enet-sys
     rustup
-    rust-analyzer
     yamllint
   ];
   # needed for enet-sys crate so bindgen can find libclang.so
